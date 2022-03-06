2628f309cc257886d88ff3a2689a0409570b8543
alternacrow <k.horita21+github@gmail.com>
2022-03-06 20:02:07 +0900

    Feature add atoms components

---
+++ src/components/atoms/FontAwesome.tsx
@@ -0,0 +1,34 @@
+import React, {FC} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {IconProp} from '@fortawesome/fontawesome-svg-core';
+import {
+  faTwitter,
+  faGithub,
+  faGitAlt,
+} from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
+import {faHome} from '@fortawesome/free-solid-svg-icons';
+
+type IconKind = 'github' | 'repository' | 'twitter' | 'email' | 'home';
+
+type Props = {
+  kind: IconKind;
+  className?: string;
+};
+
+export const FontAwesome: FC<Props> = ({kind, className}) => {
+  const icon: IconProp =
+    kind === 'github'
+      ? faGithub
+      : kind === 'repository'
+      ? faGitAlt
+      : kind === 'twitter'
+      ? faTwitter
+      : kind === 'email'
+      ? faEnvelope
+      : kind === 'home'
+      ? faHome
+      : faGithub;
+
+  return <FontAwesomeIcon className={className} icon={icon} />;
+};
