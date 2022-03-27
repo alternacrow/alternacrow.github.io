7ba2160952fa3b41d15f60cf5b6ff953fc9a8c87
alternacrow <k.horita21+github@gmail.com>
2022-03-27 14:28:41 +0900

    Refactor _app and _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import '../src/assets/styles/global.scss';
 import {du} from '../src/utils';
 du.setupDayjs();
 
-import type {AppProps} from 'next/app';
+import App from 'next/app';
 
-function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+class MyApp extends App {
+  render(): JSX.Element {
+    const {Component, pageProps} = this.props;
+
+    return <Component {...pageProps} />;
+  }
 }
 
 export default MyApp;
