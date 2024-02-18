5e3ad74961a9b173dbacfc167e5c5ea90d84ba4b
alternacrow <alternacrow@users.noreply.github.com>
2024-02-18 11:11:46 +0000

    deploy: 12487455b5ac60cacb7146367d18f6178d62bbf8

---
+++ _next/static/chunks/app/(products)/app-privacy-policy/page-cc8a097f258a49eb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[934],{5473:function(n,e,u){Promise.resolve().then(u.t.bind(u,7260,23))},7260:function(){}},function(n){n.O(0,[512,184,744],function(){return n(n.s=5473)}),_N_E=n.O()}]);
