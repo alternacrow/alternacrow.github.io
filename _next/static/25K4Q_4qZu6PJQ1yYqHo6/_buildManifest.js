389df4f8797a0cd831f41df420f2063278046a84
alternacrow <alternacrow@users.noreply.github.com>
2024-10-29 13:42:54 +0000

    deploy: 47bfca60472f60c48ed71a49ed8e3d4e34ab1f2d

--- _next/static/25K4Q_4qZu6PJQ1yYqHo6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-18d89dbbd7a7bae0.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/chunks/app/(products)/app-privacy-policy/page-f2bf59e81ebe33cf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[934],{2063:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6249,23))},6249:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[678,158,314,744],()=>s(2063)),_N_E=e.O()}]);
